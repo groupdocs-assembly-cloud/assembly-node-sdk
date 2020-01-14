50664fb740b20c376ac0fae71d5625a1292cbb6e
ivan.kishchenko <ivan.kishchenko@aspose.com>
2020-01-14 13:04:41 +0300

    Update sdk codegen files and fix tests

--- src/api.ts
+++ src/api.ts
@@ -21,18 +21,15 @@
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 * SOFTWARE.
 */
-
-import http = require("http");
 import request = require("request");
 
-import { AssemblyApiAvailiableVersions } from "./internal/assemblyApiAvailiableVersions";
+import { AssemblyApiAvailiableVersions } from "../src/internal/assemblyApiAvailiableVersions";
 import { Configuration } from "./internal/configuration";
 import { ObjectSerializer } from "./internal/objectSerializer";
 import { addQueryParameterToUrl, invokeApiMethod } from "./internal/requestHelper";
 import * as model from "./model/model";
 
 export * from "./model/model";
-export * from "./internal/assemblyApiAvailiableVersions";
 
 /**
  * Library for communicating with the GroupDocs.Assembly Cloud API
@@ -48,7 +45,6 @@ export class AssemblyApi {
      * @param appKey App key.
      * @param baseUrl Base api Url.
      * @param debugMode A value indicating whether debug mode. In debug mode all requests and responses are logged to console.
-     * @param version API version.
      */
     constructor(appSID: string, appKey: string, baseUrl?: string, debugMode?: boolean, version?: AssemblyApiAvailiableVersions) {
         this.configuration = new Configuration(appSID, appKey, baseUrl, debugMode, version);
@@ -58,35 +54,48 @@ export class AssemblyApi {
      * Builds a document using document template and XML or JSON data passed in request
      * @param requestObj contains request parameters
      */
-    // tslint:disable-next-line:completed-docs
-    public async postAssembleDocument(requestObj: model.PostAssembleDocumentRequest): Promise<{response: http.IncomingMessage, body: Buffer}> {
+    public async postAssembleDocument(requestObj: model.PostAssembleDocumentRequest): Promise<model.GroupDocsIncomingMessage<Buffer>> {
         if (requestObj === null || requestObj === undefined) {
             throw new Error('Required parameter "requestObj" was null or undefined when calling postAssembleDocument.');
         }
 
         let localVarPath = this.configuration.getApiBaseUrl() + "/assembly/{name}/build"
-            .replace("{" + "name" + "}", String(requestObj.name));
+            .replace("/{" + "name" + "}", (requestObj.name !== null) ? "/" + String(requestObj.name) : "");
         const queryParameters: any = {};
         const formParams: any = {};
 
-        // verify required parameter 'requestObj.name' is not null or undefined
-        if (requestObj.name === null || requestObj.name === undefined) {
-            throw new Error('Required parameter "requestObj.name" was null or undefined when calling postAssembleDocument.');
+        // verify required parameter 'requestObj.name' is not undefined
+        if (requestObj.name === undefined) {
+            throw new Error('Required parameter "requestObj.name" was undefined when calling postAssembleDocument.');
         }
 
-        // verify required parameter 'requestObj.data' is not null or undefined
-        if (requestObj.data === null || requestObj.data === undefined) {
-            throw new Error('Required parameter "requestObj.data" was null or undefined when calling postAssembleDocument.');
+        // verify required parameter 'requestObj.name' is not null
+        if (requestObj.name === null) {
+            throw new Error('Required parameter "requestObj.name" was null when calling postAssembleDocument.');
         }
 
-        // verify required parameter 'requestObj.saveOptions' is not null or undefined
-        if (requestObj.saveOptions === null || requestObj.saveOptions === undefined) {
-            throw new Error('Required parameter "requestObj.saveOptions" was null or undefined when calling postAssembleDocument.');
+        // verify required parameter 'requestObj.data' is not undefined
+        if (requestObj.data === undefined) {
+            throw new Error('Required parameter "requestObj.data" was undefined when calling postAssembleDocument.');
+        }
+
+        // verify required parameter 'requestObj.data' is not null
+        if (requestObj.data === null) {
+            throw new Error('Required parameter "requestObj.data" was null when calling postAssembleDocument.');
+        }
+
+        // verify required parameter 'requestObj.saveOptions' is not undefined
+        if (requestObj.saveOptions === undefined) {
+            throw new Error('Required parameter "requestObj.saveOptions" was undefined when calling postAssembleDocument.');
+        }
+
+        // verify required parameter 'requestObj.saveOptions' is not null
+        if (requestObj.saveOptions === null) {
+            throw new Error('Required parameter "requestObj.saveOptions" was null when calling postAssembleDocument.');
         }
         
         localVarPath = addQueryParameterToUrl(localVarPath, queryParameters, "folder", requestObj.folder);
         localVarPath = addQueryParameterToUrl(localVarPath, queryParameters, "destFileName", requestObj.destFileName);
-        formParams.saveOptions = JSON.stringify(requestObj.saveOptions);
         if (requestObj.data !== undefined) {
             formParams.Data = requestObj.data;
         }
@@ -95,13 +104,15 @@ export class AssemblyApi {
             method: "POST",
             qs: queryParameters,
             uri: localVarPath,
-            formData: formParams,
             encoding: null,
+            body: ObjectSerializer.serialize(requestObj.saveOptions, requestObj.saveOptions.constructor.name === "Object" ? "LoadSaveOptionsData" : requestObj.saveOptions.constructor.name),
         };
 
+        requestOptions.formData = formParams;        
         const response = await invokeApiMethod(requestOptions, this.configuration);
-        const result =  ObjectSerializer.deserialize(response.body, "Buffer");
-        return Promise.resolve({body: result, response});
+        const body =  ObjectSerializer.deserialize(response.body, "Buffer");
+        const result: model.GroupDocsIncomingMessage<Buffer> = {body, response};
+        return Promise.resolve(result);
     }
 
 }
